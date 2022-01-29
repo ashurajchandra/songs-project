be469c1be159331fe0f6a61f74283da25f66e114
Ashutosh rai <67899702+ashurajchandra@users.noreply.github.com>
2022-01-29 22:52:56 +0530

    added functionality to select song from list

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {selectSong} from '../actions'
+
  function SongList(props) {
     console.log("props",props);
   function renderList(){
@@ -7,12 +9,15 @@ import {connect} from 'react-redux';
             return(
                 <div className='item' key={song.title}>
                     <div className='right floated content'>
-                        <button className='ui button primary'>
+                        <button className='ui button primary' onClick={()=>props.selectSong(song)}>
                             Select
                         </button>
                     </div>
                     <div className='content'>
                         {song.title}
+                    </div>
+                    <div>
+                    <b>{song.title ===props.favoriteTitle && 'Favorite!'}</b>
                     </div>
                 </div>
             )
@@ -27,4 +32,4 @@ const mapStateToProps =state=>{
     console.log("state",state);
 return {songs:state.songs}
 }
-export default connect(mapStateToProps)(SongList)
+export default connect(mapStateToProps,{selectSong})(SongList)
